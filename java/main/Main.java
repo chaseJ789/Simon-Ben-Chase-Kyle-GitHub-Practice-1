5b037bb2f98524572efbeb764e5945db304916ed
Chase Johnson <johnsonc93@xavier.edu>
2026-04-19 20:08:47 -0400

    I made a black joke

--- java/main/Main.java
+++ java/main/Main.java
@@ -1,5 +1,6 @@
 //main for jokes
 
-public static void main(String[] args) {
-
+public static void main(String[] args)
+{
+    System.out.println("What TikTok famous food reviewer left the city of Atlanta in shambles? Keith Lee.");
 }
