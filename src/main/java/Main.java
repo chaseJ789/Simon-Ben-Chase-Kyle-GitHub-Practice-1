3af20e338b0f2666e0da67d781b3f708ce2b0e25
Chase Johnson <johnsonc93@xavier.edu>
2026-04-27 02:59:30 -0400

    I added another black joke

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -60,6 +60,27 @@ public class Main {
 
         System.out.println("Keith Lee");
     }
+
+    public static void BeepBoop()
+    {
+        System.out.println("What do Black people get on December 21?");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("What?");
+
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Superpowers");
+    }
 }
     public static void main(String[] args) {
         // Add code to print "Hello, world!" here
